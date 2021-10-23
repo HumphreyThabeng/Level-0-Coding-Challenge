9d587c2fd3ce40b5ada8ea5d152d11baeb62096d
HumphreyThabeng <91993837+HumphreyThabeng@users.noreply.github.com>
2021-10-23 12:54:41 +0200

    Delete Task 0.7 Celcius to Fahrenheit.py

--- Task 0.7 Celcius to Fahrenheit.py
+++
@@ -1,9 +0,0 @@
-c = int(input("Enter Temperature in Celcius: "))
-
-
-def celcius_conv(c):
-    fahrenheit = (c * 9 / 5) + 32
-    print(fahrenheit)
-
-print("The in Fahrenheit is ")
-celcius_conv(c)
